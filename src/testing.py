298f10ab59b576a0e67cb215027c35f8c7aa0d17
Kevin <monahank@uwm.edu>
2024-02-06 13:36:04 -0600

    Recommendation Testing

--- src/testing.py
+++ src/testing.py
@@ -1,10 +1,22 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-read-private"
+scope = ("user-read-playback-state user-modify-playback-state user-read-currently-playing streaming "
+         "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public "
+         "user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played "
+         "user-library-modify user-library-read user-read-email user-read-private")
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 membership = sp.current_user()["product"]
-print(membership)
+if membership != "premium":
+    print("You are not authorized to access this")
+else:
+    results = sp.current_user_recently_played(limit=1)
+    artist_uri = [(results["items"][0]["track"]["artists"][0]["uri"]).split(":", 3)[2]]
+    track_uri = [(results["items"][0]["track"]["uri"]).split(":", 3)[2]]
+    genres = ["progressive bluegrass"]
+    rec = sp.recommendations(seed_artists=artist_uri, seed_tracks=track_uri, seed_genres=genres, limit=1)
+    print(rec["tracks"][0]["name"])
+
 
