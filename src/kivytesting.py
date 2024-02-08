75aae1902260c387430d71dcca8568034a78884e
mbert32 <bertherm@uwm.edu>
2024-02-08 13:37:19 -0600

    Kivy Testing KV files
    Added kv style file for login screen. implemented ability to pull text from text input files

--- src/kivytesting.py
+++ src/kivytesting.py
@@ -1,27 +1,28 @@
+import kivy
+kivy.require('2.3.0')
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
+from kivy.lang.builder import Builder
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
-class LoginScreen(GridLayout):
+class LoginScreen(Widget):
+    username = ObjectProperty(None)
+    password = ObjectProperty(None)
 
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+    def check_status(self):
+        #print('button state is: {state}'.format(state=btn.state))
+        print('username input text is: {txt}'.format(txt=self.username.text))
+        print('password input text is: {txt}'.format(txt=self.password.text))
 
-
-class MyApp(App):
-
+class SpotiVibe(App):
     def build(self):
         return LoginScreen()
-
-
+    
 if __name__ == '__main__':
-    MyApp().run()
+    Builder.load_file('./src/kv_style/kivytesting.kv')
+    SpotiVibe().run()
