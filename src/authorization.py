425cf5f1376a088e33c3c61259e19758e3d75ddf
Kevin <monahank@uwm.edu>
2024-02-16 11:40:02 -0600

    Can now take user input in the console on reruns

--- src/authorization.py
+++ src/authorization.py
@@ -1,13 +1,18 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-read-currently-playing user-read-recently-played user-library-read streaming user-modify-playback-state"
+scope = (
+    "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public "
+    "user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read"
+    "user-follow-modify user-follow-read")
 
 # note: redirect URI needs to have a port and be http, not https
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_recently_played(limit=20)
+results = sp.current_user_followed_artists(limit=20)
+print(results)
 print("Test: User's Recently Played")
 for idx, item in enumerate(results['items']):
     track = item['track']
-    print(idx+1, ": ", track['artists'][0]['name'], " - ", track['name'])
+    print(track)
+    print(idx + 1, ": ", track['artists'][0]['name'], " - ", track['name'])
--- src/testing.py
+++ src/testing.py
@@ -2,12 +2,12 @@ import os
 import kivy
 import spotipy
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.widget import Widget
 from spotipy.oauth2 import SpotifyOAuth
 from kivy.app import App
-kivy.require('2.3.0')
 
+kivy.require('2.3.0')
 
 scope = ("user-read-playback-state user-modify-playback-state user-read-currently-playing streaming "
          "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public "
@@ -15,15 +15,16 @@ scope = ("user-read-playback-state user-modify-playback-state user-read-currentl
          "user-library-modify user-library-read user-read-email user-read-private")
 
 
-def spotify_rec():
+def spotify_rec(track):
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
     membership = sp.current_user()["product"]
     if membership != "premium":
         print("You are not authorized to access this")
     else:
-        results = sp.current_user_recently_played(limit=1)
-        artist_uri = [(results["items"][0]["track"]["artists"][0]["uri"]).split(":", 3)[2]]
-        track_uri = [(results["items"][0]["track"]["uri"]).split(":", 3)[2]]
+        results = sp.search(q="track:"+track, type="track")
+
+        artist_uri = [(results["tracks"]["items"][0]["artists"][0]["uri"]).split(":", 3)[2]]
+        track_uri = [(results["tracks"]["items"][0]["uri"]).split(":", 3)[2]]
         artistinfo = sp.artist(artist_uri[0])
         genres = artistinfo["genres"]
         rec = sp.recommendations(seed_artists=artist_uri, seed_tracks=track_uri, seed_genres=[genres[0]], limit=1)
@@ -42,7 +43,10 @@ class RecommendationsTest(Widget):
 class RecommendationApp(App):
 
     def build(self):
-        image, artist_name, track_name = spotify_rec()
+        cwd = os.getcwd()
+        self.root = Builder.load_file(cwd + '/kv_style/RecommendationsTest.kv')
+        track = input("Enter a Song Name")
+        image, artist_name, track_name = spotify_rec(track)
         app = RecommendationsTest()
         app.image_source = image
         app.Artist_text = artist_name
@@ -53,7 +57,6 @@ class RecommendationApp(App):
         self.stop()  # Stop the current app instance
         RecommendationApp().run()
 
+
 if __name__ == "__main__":
-    cwd = os.getcwd()
-    Builder.load_file(cwd + '/kv_style/RecommendationsTest.kv')
     RecommendationApp().run()
