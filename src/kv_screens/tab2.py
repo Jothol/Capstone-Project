8b71d45d2f28b7a83a86ed53f2d9e604328f6a92
wardre <wardre@uwm.edu>
2024-03-07 14:00:24 -0600

    last-minute changes to account and session fields with bugs fixed in kv and py files

--- src/kv_screens/tab2.py
+++ src/kv_screens/tab2.py
@@ -1,4 +1,7 @@
 import kivy
+from kivy.animation import Animation
+from kivy.graphics import RoundedRectangle, Color
+from kivy.metrics import dp
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 
@@ -7,6 +10,42 @@ kivy.require('2.3.0')
 
 class Tab2(Screen):
     index = 2
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_widget(Label(text="Tab 2!"))
+
+    def restart(self):
+        pass
+
+    def play(self):
+        if self.ids.play_icon.source == '../other/images/play_icon.png':
+            self.ids.play_icon.source = '../other/images/pause_icon.png'
+        else:
+            self.ids.play_icon.source = '../other/images/play_icon.png'
+
+    def skip(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
+
+    def animate_player(self):
+        player_window = self.ids.player_window
+        control_buttons = self.ids.control_buttons
+
+        if player_window.y < -250:
+            animation_controls = Animation(pos=(control_buttons.x, control_buttons.y + dp(50)), duration=0.1)
+            animation_window = Animation(pos=(player_window.x, player_window.y + dp(400)), duration=0.1)
+
+        else:
+            animation_window = Animation(pos=(player_window.x, player_window.y - dp(400)), duration=0.1)
+            animation_controls = Animation(pos=(control_buttons.x, control_buttons.y - dp(50)), duration=0.1)
+
+        animation_window.start(player_window)
+        animation_controls.start(control_buttons)
--- src/main.py
+++ src/main.py
@@ -14,6 +14,9 @@ from firebase_admin import credentials
 from src.kv_screens.recommendation import RecommendationScreen
 from src.kv_screens.recommendation_input import RecommendationInputScreen
 
+from src.kv_screens.session_home import SessionHomeScreen
+from src.kv_screens.listening_session import ListeningSessionScreen
+
 
 class Spotivibe(App):
 
@@ -22,12 +25,15 @@ class Spotivibe(App):
         sm = ScreenManager()
         sm.ids.username = ''
         sm.ids.recInput = ''
+        sm.ids.session_name = ''
         sm.add_widget(LoginScreen(name='login_page'))
         sm.add_widget(CreateAccount(name='create_account_page'))
         sm.add_widget(AddAccountInfo(name='add_account_info_page'))
         sm.add_widget(HomeScreen(name='home_page'))
         sm.add_widget(RecommendationScreen(name='recommendation_page'))
         sm.add_widget(RecommendationInputScreen(name='recommendation_input_page'))
+        sm.add_widget(SessionHomeScreen(name='session_home_page'))
+        sm.add_widget(ListeningSessionScreen(name='listening_session_page'))
 
         return sm
 
@@ -42,5 +48,7 @@ if __name__ == '__main__':
     Builder.load_file("kv_style/home.kv")
     Builder.load_file("kv_style/recommendation.kv")
     Builder.load_file("kv_style/recommendation_input.kv")
+    Builder.load_file("kv_style/session_home.kv")
+    Builder.load_file("kv_style/listening_session.kv")
 
     Spotivibe().run()
