a0b35ff894717ca377bab9fb0bfef39f5b5030d6
Kevin <monahank@uwm.edu>
2024-04-01 19:29:56 -0500

    File for testing

---
+++ src/testMult.py
@@ -0,0 +1,13 @@
+import subprocess
+
+scripts = ["./main.py",
+           "./main.py"]
+
+processes = []
+
+for script in scripts:
+    command = ["python", script]
+    processes.append(subprocess.Popen(command))
+
+for process in processes:
+    process.wait()
