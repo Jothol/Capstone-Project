fd1ad988fa86576d33a2ca1bd34940df72f876cd
mbert32 <bertherm@uwm.edu>
2024-04-12 09:09:10 -0500

    chat buttons are positioned correctly and chat history appears in correct place

--- src/kv_screens/chat.py
+++ src/kv_screens/chat.py
@@ -25,24 +25,26 @@ class ScrollableLabel(ScrollView):
         self.layout = GridLayout(cols=1, size_hint_y=None)
         self.layout.bind(minimum_height=self.layout.setter('height'))
 
-        self.border = (0, 0, 0, 0)
+        '''self.border = (0, 0, 0, 0)
         with self.layout.canvas.before:
             Color(1, 1, 1, 1)
             self.chat_border = Rectangle(size=self.size, pos=self.size)
             Color(0, 0, 0, 1)
             self.chat_background = Rectangle(size=self.size, pos=self.pos)
+        '''
         self.add_widget(self.layout)
 
-        self.chat_history = Label(size_hint_y=None, markup=True, pos=self.chat_background.pos, \
-                                  size = self.size)
-        #self.scroll_to_point = Label()
+        self.chat_history = Label(size_hint_y=None, markup=True, pos=self.pos, \
+                                  size=self.size)
+        # self.scroll_to_point = Label()
 
         self.layout.add_widget(self.chat_history)
-        #self.layout.add_widget(self.scroll_to_point)
+        # self.layout.add_widget(self.scroll_to_point)
 
     def update_chat_history(self, message):
         self.chat_history.text += '\n' + message
-        self.layout.height = self.chat_history.texture_size[1] + 15
+        if self.layout.height < self.chat_history.texture_size[1] + 15:
+            self.layout.height = self.chat_history.texture_size[1] + 15
         self.chat_history.height = self.layout.height
         self.chat_history.text_size = (self.chat_history.width * 0.98, None)
 
@@ -67,22 +69,28 @@ class ChatScreen(GridLayout):
         self.rows = 3
         self.session_name = session_name
         self.username = username
-        self.pos=(0, 50)
+        self.pos_hint = {'bottom': 0.9}
         self.height = Window.size[1]
+        with self.canvas.before:
+            self.background_color = (0, 0, 0, 1)
+            self.background_fill_color = (0, 0, 0, 1)
 
         # Create the float layout in order for ls_tab1 to place the color option button and leave chat option
-        self.chat_options = FloatLayout(height=30, width=Window.width, )
+        self.chat_options = FloatLayout(size=(Window.width, 30), pos_hint={'top': 1}, size_hint=(None, None))
         # Add to ls_tab1 when chat is engaged
-        # self.color_options = Button(text="Color", size=(100, 30), pos_hint={'left': 0.1})
-        # self.leave_chat = Button(text="Leave Chat", size=(100, 30), pos_hint={'right': 0.9})
+        self.color_options = Button(text="Color", size_hint=(None, None), size=(100, 30), pos_hint={'left': 1, 'top': 1})
+        self.leave_chat = Button(text="Leave Chat", size_hint=(None, None), size=(100, 30),
+                                 pos_hint={'right': 1, 'top': 1})
+        self.chat_options.add_widget(self.color_options)
+        self.chat_options.add_widget(self.leave_chat)
         self.add_widget(self.chat_options)
 
         # Add the scrollable history label to the grid
-        self.history = ScrollableLabel(height=Window.size[1] * 0.7, size=(Window.size[0], Window.size[1] - 50))
+        self.history = ScrollableLabel(size_hint=(1, 0.7))
         self.add_widget(self.history)
 
         # Add the send and text input to the grid
-        self.new_message = TextInput(width=Window.size[0] * 0.8, size_hint_x=None, multiline=False, \
+        self.new_message = TextInput(width=Window.size[0] * 0.8, size_hint_x=None, multiline=False,
                                      height=Window.size[1] * 0.1, size_hint_y=None)
         self.send = Button(text="Send", size_hint_x=None, size_hint_y=None, height=Window.size[1] * 0.1)
         self.send.bind(on_press=self.send_message)
