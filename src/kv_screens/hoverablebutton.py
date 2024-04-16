0e88d8dbea8cad3ad5654d8c93a37c7dd760da2a
mbert32 <bertherm@uwm.edu>
2024-04-15 22:35:51 -0500

    ability to hover the grey buttons

--- src/kv_screens/hoverablebutton.py
+++ src/kv_screens/hoverablebutton.py
@@ -2,18 +2,23 @@ from kivy.core.window import Window
 from kivy.uix.button import Button
 
 class HoverableButton(Button):
-    def __init__(self, offset=(0, 0), **kwargs):
+    def __init__(self, offset=(0, 0), transition_color="green", **kwargs):
         super().__init__(**kwargs)
         Window.bind(mouse_pos=self.on_mouseover)
         self.offset = offset
+        self.transition_color = transition_color
 
     def on_mouseover(self, window, pos):
-        print(*pos)
-        print(pos)
         adjusted_pos = (pos[0] + self.offset[0], pos[1] + self.offset[1])
         if self.collide_point(*adjusted_pos):
-            self.background_color = (0, 0.5, 0, 1)
+            if self.transition_color == "green":
+                self.background_color = (0, 0.5, 0, 1)
+            else:
+                self.background_color = (0.3, 0.3, 0.3, 1)
         else:
-            self.background_color = (0, 1, 0, 1)
+            if self.transition_color == "green":
+                self.background_color = (0, 1, 0, 1)
+            else:
+                self.background_color = (0.1, 0.1, 0.1, 1)
 
 
